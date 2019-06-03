dc86aa82d2272b9c6fdeab13a437473128a3395e
linjj93 <linjj93@hotmail.com>
2019-06-03 23:15:17 +0800

    make changes to tests

--- test/main.test.js
+++ test/main.test.js
@@ -3,19 +3,12 @@ const queueService = require("../src/queueService");
 const paymentService = require("../src/paymentService");
 
 const queueSpy = jest.spyOn(queueService, "generateQueue");
-
-test("processPayments should call generateQueue", () => {
-  const paid = processPayments();
-  expect(queueSpy).toHaveBeenCalled();
-  expect(paid).toBe(undefined);
-});
+paymentService.makePayment = jest.fn();
+paymentService.refundPayment = jest.fn();
 
 test("does not call makePayment or refundPayment when paymentQueue is empty", () => {
   // empty queue array
-
-  queueSpy.mockImplementation(() => []);
-  paymentService.makePayment = jest.fn();
-  paymentService.refundPayment = jest.fn();
+  queueSpy.mockReturnValue([]);
   const paid = processPayments();
   expect(paid).toBe(undefined);
   expect(queueSpy).toHaveBeenCalled();
@@ -29,20 +22,16 @@ test("calls makePayment when next item in paymentQueue is positive", () => {
       .fill()
       .map(num => 10)
   );
-  paymentService.makePayment = jest.fn();
   const paid = processPayments();
   expect(paymentService.makePayment).toHaveBeenCalled();
 });
 
 test("calls refundPayment when next item in paymentQueue is negative", () => {
-  // simulate negative queue items
-
   queueSpy.mockImplementation(() =>
     Array(10)
       .fill()
       .map(num => -10)
   );
-  paymentService.refundPayment = jest.fn();
   const paid = processPayments();
   expect(paymentService.refundPayment).toHaveBeenCalled();
 });
